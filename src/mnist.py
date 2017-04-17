786c99e78c9191dbec8344602b8695e1f91b9fc3
Andrey Sozykin <sozykin@gmail.com>
2017-04-17 13:06:21 +0500

    mnist.py updated to Keras 2 API

--- src/mnist.py
+++ src/mnist.py
@@ -27,8 +27,8 @@ Y_test = np_utils.to_categorical(y_test, 10)
 model = Sequential()
 
 # Добавляем уровни сети
-model.add(Dense(800, input_dim=784, init="normal", activation="relu"))
-model.add(Dense(10, init="normal", activation="softmax"))
+model.add(Dense(800, input_dim=784, activation="relu", kernel_initializer="normal"))
+model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
 
 # Компилируем модель
 model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
@@ -36,7 +36,7 @@ model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accura
 print(model.summary())
 
 # Обучаем сеть
-model.fit(X_train, Y_train, batch_size=200, nb_epoch=25, validation_split=0.2, verbose=1)
+model.fit(X_train, Y_train, batch_size=200, epochs=25, validation_split=0.2, verbose=2)
 
 # Оцениваем качество обучения сети на тестовых данных
 scores = model.evaluate(X_test, Y_test, verbose=0)
