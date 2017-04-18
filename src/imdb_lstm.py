817cfb0b97a90c0cc5ef6bf1053854057b132f4c
Andrey Sozykin <sozykin@gmail.com>
2017-04-18 10:11:03 +0500

    imdb_lstm.py updated to Keras 2 API

--- src/imdb_lstm.py
+++ src/imdb_lstm.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Embedding
-from keras.layers import LSTM
+from keras.layers import LSTM, SpatialDropout1D
 from keras.datasets import imdb
 
 # Устанавливаем seed для повторяемости результатов
@@ -13,7 +13,7 @@ max_features = 5000
 maxlen = 80
 
 # Загружаем данные
-(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=max_features)
 
 # Заполняем или обрезаем рецензии
 X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
@@ -22,9 +22,10 @@ X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
 # Создаем сеть
 model = Sequential()
 # Слой для векторного представления слов
-model.add(Embedding(max_features, 32, dropout=0.2))
+model.add(Embedding(max_features, 32))
+model.add(SpatialDropout1D(0.2))
 # Слой долго-краткосрочной памяти
-model.add(LSTM(100, dropout_W=0.2, dropout_U=0.2)) 
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2)) 
 # Полносвязный слой
 model.add(Dense(1, activation="sigmoid"))
 
@@ -34,8 +35,8 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 # Обучаем модель
-model.fit(X_train, y_train, batch_size=64, nb_epoch=7,
-          validation_data=(X_test, y_test), verbose=1)
+model.fit(X_train, y_train, batch_size=64, epochs=7,
+          validation_data=(X_test, y_test), verbose=2)
 # Проверяем качество обучения на тестовых данных
 scores = model.evaluate(X_test, y_test,
                         batch_size=64)
